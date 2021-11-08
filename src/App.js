3f2bd021f5dd51ff6055b9a62eee990fc841bcae
Dmitry <d.ramanchuk@arateg.com>
2021-11-08 19:21:02 +0300

    Fix

--- src/App.js
+++ src/App.js
@@ -8,150 +8,172 @@ import { ModalWindow } from "./components/ModalWindows/ModalWindow";
 
 import { Post } from "./components/Posts";
 import {
-	StoryForm,
-	PhotoForm,
-	VideoForm,
-	AudioForm,
-	LinkForm,
-	QuotaForm,
+  StoryForm,
+  PhotoForm,
+  VideoForm,
+  AudioForm,
+  LinkForm,
+  QuotaForm,
 } from "./components/Forms";
 import DataComponent from "./Data/DataComponent";
 
 function App() {
-	const [isVisible, setIsVisible] = useState(false);
-	const [isShown, setIsShown] = useState(false);
-	const [isOn, setIsOn] = useState(false);
-	const [isVisibleLink, setIsVisibleLink] = useState(false);
-	const [isVisibleAudio, setIsVisibleAudio] = useState(false);
-	const [isVisibleVideo, setIsVisibleVideo] = useState(false);
+  const [isVisible, setIsVisible] = useState(false);
+  const [isShown, setIsShown] = useState(false);
+  const [isOn, setIsOn] = useState(false);
+  const [isVisibleLink, setIsVisibleLink] = useState(false);
+  const [isVisibleAudio, setIsVisibleAudio] = useState(false);
+  const [isVisibleVideo, setIsVisibleVideo] = useState(false);
 
-	const [posts, setPosts] = useState([]);
+  const [posts, setPosts] = useState([]);
 
-	const addPost = (post) => {
-		localStorage.setItem("data", JSON.stringify([...posts, post]));
-		setPosts([...posts, post]);
-	};
+  const addPost = (post) => {
+    const updatedPost = { ...post, likeCount: 0 };
 
-	useEffect(() => {
-		const toLocalStorage = localStorage.getItem("data");
-		const dataParsed = toLocalStorage ? JSON.parse(toLocalStorage) : [];
-		setPosts(dataParsed);
-	}, []);
+    localStorage.setItem("data", JSON.stringify([...posts, updatedPost]));
+    setPosts([...posts, updatedPost]);
+  };
 
-	return (
-		<div className={styles.app}>
-			<div className={styles.app_header}>
-				<h1>STAYINFORMED</h1>
-			</div>
-			<div className={styles.paragraph}>
-				<p>You can always be informed with the help of my React-app.</p>
-			</div>
-			<div className={styles.paragraph_by}>
-				<p>Made by Patskevich Darya.</p>
-			</div>
-			<div className={styles.data}>
-				<DataComponent data={new Date()} />
-			</div>
-			<div className={styles.buttons}>
-				<Button
-					className={styles.text}
-					img={"./images/text.svg"}
-					onClick={() => {
-						setIsVisible(true);
-					}}
-				/>
-				<Button
-					className={styles.photo}
-					img={"./images/photo.svg"}
-					onClick={() => {
-						setIsShown(true);
-					}}
-				/>
-				<Button
-					className={styles.quota}
-					img={"./images/quota.svg"}
-					onClick={() => {
-						setIsOn(true);
-					}}
-				/>
-				<Button
-					className={styles.link}
-					img={"./images/link.svg"}
-					onClick={() => {
-						setIsVisibleLink(true);
-					}}
-				/>
-				<Button
-					className={styles.audio}
-					img={"./images/audio.svg"}
-					onClick={() => {
-						setIsVisibleAudio(true);
-					}}
-				/>
-				<Button
-					className={styles.video}
-					img={"./images/video.svg"}
-					onClick={() => {
-						setIsVisibleVideo(true);
-					}}
-				/>
-			</div>
+  useEffect(() => {
+    const toLocalStorage = localStorage.getItem("data");
+    const dataParsed = toLocalStorage ? JSON.parse(toLocalStorage) : [];
+    setPosts(dataParsed);
+  }, []);
 
-			<div className={styles.posts}>
-				{posts.map((item) => (
-					<Post
-						text={item.text}
-						title={item.title}
-						img={item.img}
-						videoLink={item.videoLink}
-						audioLink={item.audioLink}
-						author={item.author}
-						webLink={item.webLink}
-						key={item.id}
-					></Post>
-				))}
-			</div>
-			<ModalWindow onClose={() => setIsVisible(false)} isVisible={isVisible}>
-				<StoryForm onClickAdd={addPost} closeForm={() => setIsVisible(false)} />
-			</ModalWindow>
+  const onClickDelete = (id) => {
+    const newPosts = posts.filter((item) => item.id !== id);
+    localStorage.setItem("data", JSON.stringify(newPosts));
 
-			<ModalWindow onClose={() => setIsOn(false)} isVisible={isOn}>
-				<QuotaForm onClickAdd={addPost} closeForm={() => setIsOn(false)} />
-			</ModalWindow>
+    setPosts(newPosts);
+  };
 
-			<ModalWindow onClose={() => setIsShown(false)} isVisible={isShown}>
-				<PhotoForm closeForm={() => setIsShown(false)} onClickAdd={addPost} />
-			</ModalWindow>
+  const onClickLike = (id) => {
+    const newPosts = posts.map((item) =>
+      item.id !== id ? { ...item, likeCount: item.likeCount + 1 } : item
+    );
+    localStorage.setItem("data", JSON.stringify(newPosts));
 
-			<ModalWindow
-				onClose={() => setIsVisibleLink(false)}
-				isVisible={isVisibleLink}
-			>
-				<LinkForm
-					onClickAdd={addPost}
-					closeForm={() => setIsVisibleLink(false)}
-				/>
-			</ModalWindow>
-			<ModalWindow
-				onClose={() => setIsVisibleAudio(false)}
-				isVisible={isVisibleAudio}
-			>
-				<AudioForm
-					onClickAdd={addPost}
-					closeForm={() => setIsVisibleAudio(false)}
-				/>
-			</ModalWindow>
-			<ModalWindow
-				onClose={() => setIsVisibleVideo(false)}
-				isVisible={isVisibleVideo}
-			>
-				<VideoForm
-					onClickAdd={addPost}
-					closeForm={() => setIsVisibleVideo(false)}
-				/>
-			</ModalWindow>
-		</div>
-	);
+    setPosts(newPosts);
+  };
+
+  return (
+    <div className={styles.app}>
+      <div className={styles.app_header}>
+        <h1>STAYINFORMED</h1>
+      </div>
+      <div className={styles.paragraph}>
+        <p>You can always be informed with the help of my React-app.</p>
+      </div>
+      <div className={styles.paragraph_by}>
+        <p>Made by Patskevich Darya.</p>
+      </div>
+      <div className={styles.data}>
+        <DataComponent data={new Date()} />
+      </div>
+      <div className={styles.buttons}>
+        <Button
+          className={styles.text}
+          img={"./images/text.svg"}
+          onClick={() => {
+            setIsVisible(true);
+          }}
+        />
+        <Button
+          className={styles.photo}
+          img={"./images/photo.svg"}
+          onClick={() => {
+            setIsShown(true);
+          }}
+        />
+        <Button
+          className={styles.quota}
+          img={"./images/quota.svg"}
+          onClick={() => {
+            setIsOn(true);
+          }}
+        />
+        <Button
+          className={styles.link}
+          img={"./images/link.svg"}
+          onClick={() => {
+            setIsVisibleLink(true);
+          }}
+        />
+        <Button
+          className={styles.audio}
+          img={"./images/audio.svg"}
+          onClick={() => {
+            setIsVisibleAudio(true);
+          }}
+        />
+        <Button
+          className={styles.video}
+          img={"./images/video.svg"}
+          onClick={() => {
+            setIsVisibleVideo(true);
+          }}
+        />
+      </div>
+
+      <div className={styles.posts}>
+        {posts.map((item) => (
+          <Post
+            text={item.text}
+            title={item.title}
+            img={item.img}
+            videoLink={item.videoLink}
+            audioLink={item.audioLink}
+            author={item.author}
+            webLink={item.webLink}
+            key={item.id}
+            onClickDelete={onClickDelete}
+            onClickLike={onClickLike}
+            likeCount={item.likeCount}
+            id={item.id}
+          ></Post>
+        ))}
+      </div>
+      <ModalWindow onClose={() => setIsVisible(false)} isVisible={isVisible}>
+        <StoryForm onClickAdd={addPost} closeForm={() => setIsVisible(false)} />
+      </ModalWindow>
+
+      <ModalWindow onClose={() => setIsOn(false)} isVisible={isOn}>
+        <QuotaForm onClickAdd={addPost} closeForm={() => setIsOn(false)} />
+      </ModalWindow>
+
+      <ModalWindow onClose={() => setIsShown(false)} isVisible={isShown}>
+        <PhotoForm closeForm={() => setIsShown(false)} onClickAdd={addPost} />
+      </ModalWindow>
+
+      <ModalWindow
+        onClose={() => setIsVisibleLink(false)}
+        isVisible={isVisibleLink}
+      >
+        <LinkForm
+          onClickAdd={addPost}
+          closeForm={() => setIsVisibleLink(false)}
+        />
+      </ModalWindow>
+      <ModalWindow
+        onClose={() => setIsVisibleAudio(false)}
+        isVisible={isVisibleAudio}
+      >
+        <AudioForm
+          onClickAdd={addPost}
+          closeForm={() => setIsVisibleAudio(false)}
+        />
+      </ModalWindow>
+      <ModalWindow
+        onClose={() => setIsVisibleVideo(false)}
+        isVisible={isVisibleVideo}
+      >
+        <VideoForm
+          onClickAdd={addPost}
+          closeForm={() => setIsVisibleVideo(false)}
+        />
+      </ModalWindow>
+    </div>
+  );
 }
 
 export default App;
